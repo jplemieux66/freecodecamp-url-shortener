0d46e76fc4223f3beacbef78b6b5ba25b0a5acbb
Jean-Philippe Lemieux <jean-philippe.lemieux@croesus.com>
2017-07-06 12:24:12 -0400

    Add /new/* route to create new short url

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const shortid = require('shortid');
 const MongoClient = require('mongodb').MongoClient;
 
 const mongoUrl = "mongodb://freecodecamp:freecodecamp2017@ds137220.mlab.com:37220/url-shortener";
@@ -7,23 +8,28 @@ const app = express();
 
 app.get("/new/*", (req, res) => {
   MongoClient.connect(mongoUrl, function (err, db) {
-    if (err) {
-      console.log('Unable to connect to the mongoDB server. Error:', err);
-    } else {
-      console.log('Connection established to', mongoUrl);
+    if (err) throw err;
+    const originalUrl = decodeURIComponent(req.url.replace('/new/', ''));
 
-      const parameter = decodeURIComponent(req.url.substring(1));
+    const shortId = shortid.generate();
+    db.collection("urls").insertOne({
+      originalUrl,
+      shortId
+    }).catch((err) => {
+      throw err;
+    });
 
-      const responseObject = {
-
-      }
-
-      res.end(JSON.stringify(responseObject, undefined, 2));
-
-      db.close();
+    const shortUrl = `http://localhost:3000/${shortId}`;
+    const responseObject = {
+      originalUrl,
+      shortUrl
     }
-  });
 
+    res.end(JSON.stringify(responseObject, undefined, 2));
+
+    db.close();
+  });
 });
 
 app.listen(port);
+console.log(`Server is listening on port ${port}`);
